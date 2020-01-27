c7978f1c9859a75a279246adbde516eae64d469b
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2020-01-27 03:55:24 -0300

    corrigindo problema

--- passo4-user/lib-mcode3/func_plot_new_modified_image_file.m
+++ passo4-user/lib-mcode3/func_plot_new_modified_image_file.m
@@ -7,6 +7,8 @@ function [hfa IMG]=func_plot_new_modified_image_file(FILE_IMG,dat_pixel)
     %% deixo em cor preto em IMG todos os pixels correspondentes a curva
     for II=1:N
         P=dat_pixel(II,:);
+        P(1)=round(P(1));
+        P(2)=round(P(2));
         IMG(P(2),P(1),:)=0*IMG(P(2),P(1),:);
     end
 
